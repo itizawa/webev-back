c88e5267a80656b682143bcf119b7d65b1e6cc2f
itizawa <itizawa@weseek.co.jp>
2021-01-30 23:37:24 +0900

    use class

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,26 @@ import { app } from './app';
 import * as http from 'http';
 
 import * as mongoose from 'mongoose';
+import  {ExpressApp} from './services/expressApp';
 
 const PORT = 8080;
 const MONGO_URI = 'mongodb://localhost:27017/todo';
-const server = http.createServer(app);
-server.listen(PORT);
-server.on('listening', async () => {
-	console.info(`Listening on port ${PORT}`);
-	mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false,useUnifiedTopology:true});
-	mongoose.connection.on('open', () => {
-		console.info('Connected to Mongo.');
-	});
-	mongoose.connection.on('error', (err: any) => {
-		console.error(err);
-	});
-});
+// const server = http.createServer(app);
+// server.listen(PORT);
+// server.on('listening', async () => {
+// 	console.info(`Listening on port ${PORT}`);
+// 	mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false,useUnifiedTopology:true});
+// 	mongoose.connection.on('open', () => {
+// 		console.info('Connected to Mongo.');
+// 	});
+// 	mongoose.connection.on('error', (err: any) => {
+// 		console.error(err);
+// 	});
+// });
+
+
+/** **********************************
+ *          Main Process
+ ********************************** */
+
+ const expressApp = new ExpressApp()
---
+++ src/services/expressApp.ts
@@ -0,0 +1,12 @@
+
+export class ExpressApp {
+
+    app: object;
+
+    constructor(){
+       this.app = null
+       
+       console.log('huga')
+    }
+    
+}
