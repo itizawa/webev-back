f9b63e05347303f299799b0b45363700db0518a5
itizawa <itizawa@weseek.co.jp>
2021-01-31 10:48:44 +0900

    save model

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,11 +1,15 @@
 import * as express from 'express';
+import {TodoModel} from '../models/todo'
 
 const router = express.Router();
 
 export const todoRoutes = (webevApp) => {
 
-	router.get('/todo', (request: any, response: any, next: any) =>{
-		return response.status(200).json({huga:'hoge'})
+	router.get('/todo', async(request: any, response: any, next: any) =>{
+		await TodoModel.create({description:'huga'})
+		const hoge =  await TodoModel.find()
+
+		return response.status(200).json({huga:hoge})
 	});
 
 	return router;
