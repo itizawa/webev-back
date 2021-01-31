a49d9ecab67d7735605717ffffb91cf3139f971d
itizawa <itizawa@weseek.co.jp>
2021-01-31 20:22:30 +0900

    introduce rest client

--- src/routes/pages.ts
+++ src/routes/pages.ts
@@ -4,7 +4,9 @@ import { WebevApp } from '../services/WebevApp';
 const router = express.Router();
 
 export const pages = (webevApp: WebevApp): express.Router => {
-  router.get('/', async (req: express.Request, res: express.Response) => {
+  router.post('/', async (req: express.Request, res: express.Response) => {
+    const { url } = req.body;
+    console.log(url);
     webevApp.PageService.hoge();
     return res.status(200).json({ huga: 'huga' });
   });
