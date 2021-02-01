662949732b4743d3309fed25b9a412eaa9e6e7b2
itizawa <itizawa@weseek.co.jp>
2021-02-01 22:40:29 +0900

    use mongo container

--- src/services/WebevApp.ts
+++ src/services/WebevApp.ts
@@ -42,7 +42,7 @@ export class WebevApp {
   }
 
   setupDB(): Promise<typeof import('mongoose')> {
-    const MONGO_URI = 'mongodb://localhost:27017/todo';
+    const MONGO_URI = 'mongodb://mongo:27017/todo';
     return mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true });
   }
 
