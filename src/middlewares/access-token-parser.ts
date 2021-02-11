3458b9fbd54fa9db1933dc3aa88f1b4df88aa03d
itizawa <itizawa@weseek.co.jp>
2021-02-11 19:25:34 +0900

    implement accessTokenParser

---
+++ src/middlewares/access-token-parser.ts
@@ -0,0 +1,19 @@
+import { Response, NextFunction } from 'express';
+import { WebevRequest } from '../interfaces/webev-request';
+import { SessionModel } from '../models/session';
+
+const accessTokenParser = async (req: WebevRequest, res: Response, next: NextFunction): Promise<void> => {
+  const bearToken = req.headers['authorization'];
+  const bearer = bearToken.split(' ');
+  const token = bearer[1];
+
+  const user = await SessionModel.findOne({ accessToken: token }).select('userId').populate('userId');
+
+  if (user != null) {
+    req.user = user;
+  }
+
+  return next();
+};
+
+export { accessTokenParser };
