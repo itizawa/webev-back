33a612374762d3f38d3e87d5479a9e85bc1612b9
itizawa <itizawa@weseek.co.jp>
2021-03-28 19:54:56 +0900

    add route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import * as express from 'express';
 import { WebevApp } from '../services/WebevApp';
+import { directories } from './directories';
 import { pages } from './pages';
 
 export const setupExpressRoutes = (webevApp: WebevApp, express: express.Express): void => {
+  express.use('/api/v1/directories', directories(webevApp));
   express.use('/api/v1/pages', pages(webevApp));
 };
