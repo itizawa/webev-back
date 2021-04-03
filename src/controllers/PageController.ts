94f142eee424f4b2930cdfc62013edf1f79b78e6
itizawa <itizawa@weseek.co.jp>
2021-04-03 21:10:34 +0900

    refactor PageController

--- src/controllers/PageController.ts
+++ src/controllers/PageController.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@tsed/common';
-
+import { Controller, Get, Post, PathParams } from '@tsed/common';
+import { Page } from '../domains/Page';
+import { PageRepository } from '../infrastructure/PageRepository';
+import { CreatePage } from '../usecases/page/CreatePage';
 @Controller('/pages')
 export class PageController {
+  private pageRepository: PageRepository;
+
+  constructor() {
+    this.pageRepository = new PageRepository();
+  }
+
   @Get()
   findAll(): string {
     return 'This action returns all calendars';
+  }
+
+  @Post()
+  async createPage(@PathParams('title') title: string, @PathParams('description') description: string): Promise<Page> {
+    const useCase = new CreatePage(this.pageRepository);
+    const result = await useCase.execute(title, description);
+    return result;
   }
 }
