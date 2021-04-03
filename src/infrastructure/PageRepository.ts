c42ec3eb292fb287ae30ed2c29c46eb15d92aca5
itizawa <itizawa@weseek.co.jp>
2021-04-04 00:08:29 +0900

    create createPage method

--- src/infrastructure/PageRepository.ts
+++ src/infrastructure/PageRepository.ts
@@ -2,6 +2,9 @@ import { Page } from '../domains/Page';
 import { IPageRepository } from '../repositories/IPageRepository';
 import { PageModel } from '../models/page';
 export class PageRepository implements IPageRepository {
+  async createPage(page: Partial<Page>): Promise<Page> {
+    return PageModel.create(page);
+  }
   async findPageById(id: string, userId: string): Promise<Page> {
     return PageModel.findOne({ _id: id, createdUser: userId });
   }
--- src/repositories/IPageRepository.ts
+++ src/repositories/IPageRepository.ts
@@ -1,5 +1,6 @@
 import { Page } from '../domains/Page';
 
 export interface IPageRepository {
+  createPage(page: Partial<Page>): Promise<Page>;
   findPageById(id: string, userId: string): Promise<Page>;
 }
