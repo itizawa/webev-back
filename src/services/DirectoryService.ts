90d19ef0001dc5c454ef348362dc1794cc2a1d77
itizawa <itizawa@weseek.co.jp>
2021-04-09 00:09:43 +0900

    clean code

--- src/services/DirectoryService.ts
+++ src/services/DirectoryService.ts
@@ -15,17 +15,6 @@ export class DirectoryService {
     return directoryCount > 0;
   }
 
-  async saveDirectory(newDirectory: Partial<IDirectory>, userId: string): Promise<Document<IDirectory>> {
-    // Cannot use the name have already created
-    const isInDuplicate = await this.validInDuplicate(newDirectory.name, userId);
-    if (isInDuplicate) {
-      throw new Error('This name directory already exists');
-    }
-    // set creator id
-    newDirectory.createdUser = userId;
-    return DirectoryModel.create(newDirectory);
-  }
-
   async renameDirectory(directoryId: string, name: string, userId: string): Promise<Document<IDirectory>> {
     // Cannot use the name have already created
     const isInDuplicate = await this.validInDuplicate(name, userId);
