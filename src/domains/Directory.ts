0455f2ea7d28df3a16dd436960653f030c9cd597
itizawa <itizawa@weseek.co.jp>
2021-04-08 23:08:40 +0900

    implement Directory

--- src/domains/Directory.ts
+++ src/domains/Directory.ts
@@ -1,15 +1,13 @@
-export class User {
+export class Directory {
   _id: string;
   name: string;
-  email: string;
-  image: string;
+  createdUser: string;
   createdAt: Date;
   updatedAt: Date;
-  constructor(_id: string, name: string, email: string, image: string, createdAt: Date, updatedAt: Date) {
+  constructor(_id: string, name: string, createdUser: string, createdAt: Date, updatedAt: Date) {
     this._id = _id;
     this.name = name;
-    this.email = email;
-    this.image = image;
+    this.createdUser = createdUser;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
   }
