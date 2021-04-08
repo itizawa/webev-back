d9d598890c333dadc4d5abad3510f5fce3731453
itizawa <itizawa@weseek.co.jp>
2021-04-09 00:40:18 +0900

    create Session domain

---
+++ src/domains/Session.ts
@@ -0,0 +1,14 @@
+export class Session {
+  _id: string;
+  accessToken: string;
+  userId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  constructor(_id: string, accessToken: string, userId: string, createdAt: Date, updatedAt: Date) {
+    this._id = _id;
+    this.accessToken = accessToken;
+    this.userId = userId;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+}
