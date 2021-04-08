7ae4ddeabf791c0eeaf2029c4fe4d16bfa50dddd
itizawa <itizawa@weseek.co.jp>
2021-04-08 22:33:21 +0900

    implement findPageList

--- src/infrastructure/PageRepository.ts
+++ src/infrastructure/PageRepository.ts
@@ -1,12 +1,16 @@
 import { Page } from '../domains/Page';
 import { IPageRepository } from '../repositories/IPageRepository';
 import { PageModel, PageStatus } from '../models/page';
+import { PaginationQuery, PaginationOptions } from '../interfaces/pagination';
 export class PageRepository implements IPageRepository {
   async createPage(page: Partial<Page>): Promise<Page> {
     return PageModel.create(page);
   }
   async findPageById(id: string, userId: string): Promise<Page> {
     return PageModel.findOne({ _id: id, createdUser: userId });
+  }
+  async findPageList(query: PaginationQuery, options: PaginationOptions): Promise<Page> {
+    return PageModel.paginate(query, options);
   }
   async updatePageById(pageId: string, page: Partial<Page>): Promise<Page> {
     return PageModel.findByIdAndUpdate(pageId, page);
