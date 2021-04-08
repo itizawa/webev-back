a204b4714e6e0ec01c5c2645d061a887584c5a92
itizawa <itizawa@weseek.co.jp>
2021-04-08 22:38:58 +0900

    remove PageStatus

--- src/models/page.ts
+++ src/models/page.ts
@@ -1,13 +1,7 @@
 import { model, Schema, Types, Document } from 'mongoose';
 import * as mongoosePaginate from 'mongoose-paginate-v2';
-import { Page } from '../domains/Page';
+import { Page, PageStatus } from '../domains/Page';
 import { UserModel } from './user';
-
-export enum PageStatus {
-  PAGE_STATUS_STOCK = 'stocked',
-  PAGE_STATUS_ARCHIVE = 'archived',
-  PAGE_STATUS_DELETED = 'deleted',
-}
 
 const PageSchema: Schema = new Schema(
   {
