614f9f766b05b467fe08a207ebe1247c727a0e4c
itizawa <itizawa@weseek.co.jp>
2021-04-10 17:57:51 +0900

    add order

--- src/domains/Directory.ts
+++ src/domains/Directory.ts
@@ -1,12 +1,14 @@
 export class Directory {
   _id: string;
   name: string;
+  order: number;
   createdUser: string;
   createdAt: Date;
   updatedAt: Date;
-  constructor(_id: string, name: string, createdUser: string, createdAt: Date, updatedAt: Date) {
+  constructor(_id: string, name: string, order: number, createdUser: string, createdAt: Date, updatedAt: Date) {
     this._id = _id;
     this.name = name;
+    this.order = order;
     this.createdUser = createdUser;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
