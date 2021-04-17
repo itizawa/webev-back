ba9ae5adc14656f86774edcc42eb0959d0fe3996
itizawa <itizawa@weseek.co.jp>
2021-04-17 23:33:37 +0900

    refactor interface

--- src/interfaces/pagination.ts
+++ src/interfaces/pagination.ts
@@ -1,9 +1,25 @@
-export interface PaginationQuery {
-  [key: string]: string | number | boolean;
+import { PageStatus } from '../domains/Page';
+
+export class PaginationQuery {
+  createdUser: string;
+  status: PageStatus;
+  isFavorite: boolean;
+  directoryId: string;
+  constructor(createdUser: string, status: PageStatus, isFavorite?: boolean, directoryId?: string) {
+    this.createdUser = createdUser;
+    this.status = status;
+    this.isFavorite = isFavorite;
+    this.directoryId = directoryId;
+  }
 }
 
-export interface PaginationOptions {
+export class PaginationOptions {
   page: number;
   limit: number;
   sort?: { [key: string]: number };
+  constructor(page: number, limit: number, sort?: { [key: string]: number }) {
+    this.page = page;
+    this.limit = limit;
+    this.sort = sort;
+  }
 }
