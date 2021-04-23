fdaaab0e3eca01433174261c53feb4750350034a
itizawa <itizawa@weseek.co.jp>
2021-04-23 23:13:14 +0900

    implement usecase

--- src/usecases/directory/DeleteDirectory.ts
+++ src/usecases/directory/DeleteDirectory.ts
@@ -1,14 +1,18 @@
 import { Directory } from '../../domains/Directory';
 import { IDirectoryRepository } from '../../repositories/IDirectoryRepository';
+import { IPageRepository } from '../../repositories/IPageRepository';
 
 export class DeleteDirectory {
   private directoryRepository: IDirectoryRepository;
+  private pageRepository: IPageRepository;
 
-  constructor(directoryRepository: IDirectoryRepository) {
+  constructor(directoryRepository: IDirectoryRepository, pageRepository: IPageRepository) {
     this.directoryRepository = directoryRepository;
+    this.pageRepository = pageRepository;
   }
 
   async execute(directoryId: string, userId: string): Promise<Directory> {
+    await this.pageRepository.findByDirectoryIdAndDeleteDirectoryId(directoryId, userId);
     return this.directoryRepository.deleteDirectory(directoryId, userId);
   }
 }
