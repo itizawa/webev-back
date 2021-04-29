7786f9709f4b8ca9589771f78609596007cc2e4a
itizawa <itizawa@weseek.co.jp>
2021-04-29 19:18:55 +0900

    rename

--- src/repositories/IDirectoryTreeRepository.ts
+++ src/repositories/IDirectoryTreeRepository.ts
@@ -1,4 +1,4 @@
-export interface IDirectoryPathRepository {
+export interface IDirectoryTreeRepository {
   createSelfReference(directoryId: string): Promise<void>;
   createPathAsDescendant(ancestorId: string, descendantId: string): Promise<void>;
 }
