51b6706266ea0f454e63e94a364f56e5b880e77d
itizawa <itizawa@weseek.co.jp>
2021-04-30 23:43:12 +0900

    implement findAncestorDirectories

--- src/repositories/IDirectoryTreeRepository.ts
+++ src/repositories/IDirectoryTreeRepository.ts
@@ -4,4 +4,5 @@ export interface IDirectoryTreeRepository {
   createSelfReference(directoryId: string): Promise<DirectoryTree>;
   createPathAsDescendant(ancestorId: string, descendantId: string): Promise<void>;
   findChildrenDirectories(directoryId: string): Promise<DirectoryTree[]>;
+  findAncestorDirectories(directoryId: string): Promise<DirectoryTree[]>;
 }
