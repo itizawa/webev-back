a0da9473f2e5c11834a835a26939986f2ccac15b
itizawa <itizawa@weseek.co.jp>
2021-05-01 16:01:34 +0900

    remove isExisst from rename

--- src/usecases/directory/RenameDirectory.ts
+++ src/usecases/directory/RenameDirectory.ts
@@ -9,13 +9,6 @@ export class RenameDirectory {
   }
 
   async execute(directoryId: string, name: string, createdUser: string): Promise<Directory> {
-    const isExist = await this.directoryRepository.isExistDirectoryByName(name, createdUser);
-
-    // Cannot use the name have already created
-    if (isExist) {
-      throw new Error('This name directory already exists');
-    }
-
     return this.directoryRepository.renameDirectory(directoryId, name, createdUser);
   }
 }
