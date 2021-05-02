5e48a5f07f88810a12de70b7a0fec5641f3d106d
itizawa <itizawa@weseek.co.jp>
2021-05-02 14:14:47 +0900

    refactor mock file

--- src/mock/generateMockUser.ts
+++ src/mock/generateMockUser.ts
@@ -1,11 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { User } from '../domains/User';
-export const generateMockUser = (): User => {
+
+export const generateMockUser = ({ _id, name, email, image, createdAt, updatedAt }: any): User => {
   return new User({
-    _id: 'mockUserId',
-    name: 'mockUserName',
-    email: 'mockUserEmail',
-    image: 'mockUserImage',
-    createdAt: new Date('2020-01-01'),
-    updatedAt: new Date('2020-01-01'),
+    _id: _id || 'mockUserId',
+    name: name || 'mockUserName',
+    email: email || 'mockUserEmail',
+    image: image || 'mockUserImage',
+    createdAt: createdAt || new Date('2020-01-01'),
+    updatedAt: updatedAt || new Date('2020-01-01'),
   });
 };
