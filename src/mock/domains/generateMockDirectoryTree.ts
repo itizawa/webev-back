ef74af3a4748ff6c76a10d61e8629874deae5b3e
itizawa <itizawa@weseek.co.jp>
2021-05-04 21:31:15 +0900

    rename generateMockDirectoryTree

--- src/mock/domains/generateMockDirectoryTree.ts
+++ src/mock/domains/generateMockDirectoryTree.ts
@@ -1,6 +1,6 @@
 import { DirectoryTree } from '../../domains/DirectoryTree';
 
-export const generateMockDirectory = (mock: Partial<DirectoryTree> = {}): DirectoryTree => {
+export const generateMockDirectoryTree = (mock: Partial<DirectoryTree> = {}): DirectoryTree => {
   const { _id, ancestor, descendant, depth } = mock;
   return new DirectoryTree({
     _id: _id || 'mockUserId',
