e59187c74bb2c939cc4b749757140413fd48dfbf
itizawa <itizawa@weseek.co.jp>
2021-05-04 20:29:04 +0900

    fix test

--- src/mock/interfaces/generateMockPaginationQuery.ts
+++ src/mock/interfaces/generateMockPaginationQuery.ts
@@ -2,10 +2,9 @@ import { PageStatus } from '../../domains/Page';
 import { PaginationQuery } from '../../interfaces/pagination';
 
 export const generateMockPaginationQuery = (mock: Partial<PaginationQuery> = {}): PaginationQuery => {
-  const { createdUser, status, $or, isFavorite, directoryId } = mock;
+  const { createdUser, $or, isFavorite, directoryId } = mock;
   return new PaginationQuery({
     createdUser: createdUser || 'mockUserId',
-    status: status || [PageStatus.PAGE_STATUS_STOCK],
     $or: $or || [{ status: PageStatus.PAGE_STATUS_STOCK }],
     isFavorite: isFavorite || false,
     directoryId: directoryId || 'mockDirectoryId',
