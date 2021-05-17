2812e053de904869d86860b57de021a1a08343a2
itizawa <itizawa@weseek.co.jp>
2021-05-18 00:36:09 +0900

    adjust mock

--- src/mock/domains/generateMockPage.ts
+++ src/mock/domains/generateMockPage.ts
@@ -1,7 +1,7 @@
 import { Page, PageStatus } from '../../domains/Page';
 
 export const generateMockPage = (mock: Partial<Page> = {}): Page => {
-  const { _id, url, image, description, title, siteName, directoryId, createdUser, createdAt, updatedAt, status, isFavorite } = mock;
+  const { _id, url, image, description, title, siteName, directoryId, createdUser, createdAt, updatedAt, status } = mock;
   return new Page({
     _id: _id || 'mockUserId',
     url: url || 'https://github.com/itizawa/webev-front',
@@ -14,6 +14,5 @@ export const generateMockPage = (mock: Partial<Page> = {}): Page => {
     createdAt: createdAt || new Date('2020-01-01'),
     updatedAt: updatedAt || new Date('2020-01-01'),
     status: status || PageStatus.PAGE_STATUS_STOCK,
-    isFavorite: isFavorite || false,
   });
 };
