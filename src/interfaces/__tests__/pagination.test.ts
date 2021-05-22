4abd35cfda73a04f589b39995b0dd81f25baf301
itizawa <itizawa@weseek.co.jp>
2021-05-22 11:43:37 +0900

    implement PaginationDirectoryQuery

--- src/interfaces/__tests__/pagination.test.ts
+++ src/interfaces/__tests__/pagination.test.ts
@@ -1,6 +1,6 @@
 import { PageStatus } from '../../domains/Page';
 import { generateMockDirectory, generateMockUser } from '../../mock/domains';
-import { PaginationQuery, PaginationOptions } from '../pagination';
+import { PaginationQuery, PaginationOptions, PaginationDirectoryQuery } from '../pagination';
 
 describe('Pagination interface test', () => {
   const mockUser = generateMockUser();
@@ -31,5 +31,22 @@ describe('Pagination interface test', () => {
     expect(response.page).toBe(1);
     expect(response.limit).toBe(20);
     expect(response.sort).toEqual(undefined);
+  });
+
+  test('PaginationDirectoryQuery with isRoot is true', async () => {
+    const response = new PaginationDirectoryQuery({ createdUser: mockUser._id, isRoot: true });
+    expect(response.createdUser).toBe(mockUser._id);
+    expect(response.isRoot).toBe(true);
+  });
+
+  test('PaginationDirectoryQuery with isRoot is false', async () => {
+    const response = new PaginationDirectoryQuery({ createdUser: mockUser._id, isRoot: false });
+    expect(response.createdUser).toBe(mockUser._id);
+    expect(response.isRoot).toBe(false);
+  });
+  test('PaginationDirectoryQuery without isRoot', async () => {
+    const response = new PaginationDirectoryQuery({ createdUser: mockUser._id });
+    expect(response.createdUser).toBe(mockUser._id);
+    expect(response.isRoot).toBe(undefined);
   });
 });
