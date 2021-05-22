72175e4e3ec0d116f009b8c3a040163dbe4111a3
itizawa <itizawa@weseek.co.jp>
2021-05-23 01:15:23 +0900

    implement DirectoryTreeRepositoryMock

--- src/mock/repositories/__tests__/directoryTreeRepositoryMock.test.ts
+++ src/mock/repositories/__tests__/directoryTreeRepositoryMock.test.ts
@@ -1,9 +1,49 @@
 import { DirectoryTreeRepositoryMock } from '../DirectoryTreeRepositoryMock';
+import { generateMockDirectory, generateMockDirectoryTree } from '../../../mock/domains';
 
 describe('DirectoryTreeRepositoryMock test', () => {
-  test('DirectoryTreeRepositoryMock', async () => {
-    const directoryTreeRepositoryMock = new DirectoryTreeRepositoryMock();
+  const mockDirectory = generateMockDirectory();
+  const mockDirectoryTree = generateMockDirectoryTree();
 
-    expect(directoryTreeRepositoryMock).toEqual({});
+  const directoryTreeRepositoryMock = new DirectoryTreeRepositoryMock();
+
+  test('createSelfReference', async () => {
+    try {
+      await directoryTreeRepositoryMock.createSelfReference(mockDirectory._id);
+    } catch (e) {
+      expect(e).toEqual(new Error('Method not implemented.'));
+    }
+  });
+
+  test('createPathAsDescendant', async () => {
+    try {
+      await directoryTreeRepositoryMock.createPathAsDescendant(mockDirectoryTree.ancestor, mockDirectoryTree.descendant);
+    } catch (e) {
+      expect(e).toEqual(new Error('Method not implemented.'));
+    }
+  });
+
+  test('findChildrenDirectories', async () => {
+    try {
+      await directoryTreeRepositoryMock.findChildrenDirectories(mockDirectory._id);
+    } catch (e) {
+      expect(e).toEqual(new Error('Method not implemented.'));
+    }
+  });
+
+  test('findAncestorDirectories', async () => {
+    try {
+      await directoryTreeRepositoryMock.findAncestorDirectories(mockDirectory._id);
+    } catch (e) {
+      expect(e).toEqual(new Error('Method not implemented.'));
+    }
+  });
+
+  test('deleteDirectoryTree', async () => {
+    try {
+      await directoryTreeRepositoryMock.deleteDirectoryTree(mockDirectory._id);
+    } catch (e) {
+      expect(e).toEqual(new Error('Method not implemented.'));
+    }
   });
 });
