dc2c785ddf331c437b6132bec37917f67a07e582
itizawa <itizawa@weseek.co.jp>
2021-05-30 00:08:54 +0900

    implement test

---
+++ src/mock/repositories/__tests__/inquiryRepositoryMock.test.ts
@@ -0,0 +1,16 @@
+import { InquiryRepositoryMock } from '../InquiryRepositoryMock';
+import { generateMockInquiry } from '../../domains';
+
+describe('InquiryRepositoryMock test', () => {
+  const mockInquiry = generateMockInquiry();
+
+  const inquiryRepositoryMock = new InquiryRepositoryMock();
+
+  test('postInquiry', async () => {
+    try {
+      await inquiryRepositoryMock.postInquiry(mockInquiry);
+    } catch (e) {
+      expect(e).toEqual(new Error('Method not implemented.'));
+    }
+  });
+});
