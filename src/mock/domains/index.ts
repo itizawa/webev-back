d750860a13e56a3ea5a74e31775758e852f5212d
itizawa <itizawa@weseek.co.jp>
2021-05-29 23:33:30 +0900

    export generateMockInquiry

--- src/mock/domains/index.ts
+++ src/mock/domains/index.ts
@@ -1,4 +1,5 @@
 export { generateMockDirectory } from './generateMockDirectory';
 export { generateMockDirectoryTree } from './generateMockDirectoryTree';
+export { generateMockInquiry } from './generateMockInquiry';
 export { generateMockPage } from './generateMockPage';
 export { generateMockUser } from './generateMockUser';
