63f747883f12680426fa27449d4d04491df21b4b
itizawa <itizawa@weseek.co.jp>
2021-05-29 23:31:59 +0900

    rename

--- src/mock/repositories/InquiryRepositoryMock.ts
+++ src/mock/repositories/InquiryRepositoryMock.ts
@@ -2,7 +2,7 @@
 import { Inquiry } from '../../domains/Inquiry';
 import { IInquiryRepository } from '../../repositories/IInquiryRepository';
 
-export class InquiryRepository implements IInquiryRepository {
+export class InquiryRepositoryMock implements IInquiryRepository {
   postInquiry({ type, email, text }: Partial<Inquiry>): Promise<Inquiry> {
     throw new Error('Method not implemented.');
   }
