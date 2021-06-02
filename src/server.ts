c69d25c27588602c4a3c558d9e5479674a63fbe0
itizawa <itizawa@weseek.co.jp>
2021-06-02 22:52:35 +0900

    implement server

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import { Configuration, Inject, PlatformApplication } from '@tsed/common';
+import * as cookieParser from 'cookie-parser';
+
+const rootDir = __dirname;
+
+@Configuration({
+  rootDir,
+  acceptMimes: ['application/json'],
+})
+export class Server {
+  @Inject()
+  app: PlatformApplication;
+
+  @Configuration()
+  settings: Configuration;
+
+  /**
+   * This method let you configure the express middleware required by your application to works.
+   * @returns {Server}
+   */
+  public $beforeRoutesInit(): void | Promise<any> {
+    this.app.use(cookieParser());
+  }
+}
