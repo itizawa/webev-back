68f637eb995654fae3c45a7534373c08be0e79cb
itizawa <itizawa@weseek.co.jp>
2021-06-05 20:50:57 +0900

    adjust message

--- src/services/SlackNotificationService.ts
+++ src/services/SlackNotificationService.ts
@@ -7,7 +7,7 @@ export class SlackNotificationService {
       const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
       await webhook.send({
         icon_emoji: 'email',
-        text: `${inquiry.email || 'No Email'} からお問い合わせが届きました。\n **Type ${inquiry.type}** \n ${inquiry.text}`,
+        text: `${inquiry.email || 'No Email'} からお問い合わせが届きました。\n Type: ${inquiry.type} \n ${inquiry.text}`,
       });
     } catch (error) {
       console.log(error);
