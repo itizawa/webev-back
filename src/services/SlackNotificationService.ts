3659449339616cc8ba98ccc7b13cfa1ba2f23bf6
itizawa <itizawa@weseek.co.jp>
2021-06-05 20:47:00 +0900

    send inquiry

--- src/services/SlackNotificationService.ts
+++ src/services/SlackNotificationService.ts
@@ -1,9 +1,14 @@
+import { IncomingWebhook } from '@slack/webhook';
 import { Inquiry } from '../domains/Inquiry';
 
 export class SlackNotificationService {
   async notifyInquiryToSlack(inquiry: Inquiry): Promise<void> {
     try {
-      console.log(inquiry);
+      const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
+      await webhook.send({
+        icon_emoji: 'email',
+        text: `${inquiry.email || 'No Email'} からお問い合わせが届きました。\n **Type ${inquiry.type}** \n ${inquiry.text}`,
+      });
     } catch (error) {
       console.log(error);
     }
