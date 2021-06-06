30666178858d6a1adcd17f0a7d8ca9e60f90792c
zahmis <meda829ratteru86@gmail.com>
2021-06-06 18:13:43 +0900

    add accessTokenParser

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,7 +2,7 @@ import { Router, Response } from 'express';
 import { body, param, query } from 'express-validator';
 // import { apiValidatorMiddleware } from '../middlewares/api-validator';
 // import { loginRequired } from '../middlewares/login-required';
-// import { accessTokenParser } from '../middlewares/access-token-parser';
+import { accessTokenParser } from '../middlewares/access-token-parser';
 
 import { WebevApp } from '../services/WebevApp';
 import { WebevRequest } from '../interfaces/webev-request';
@@ -17,7 +17,7 @@ export const users = (webevApp: WebevApp): Router => {
   /**
    * @swagger
    */
-  router.get('/me', async (req: WebevRequest, res: Response) => {
+  router.get('/me', accessTokenParser, async (req: WebevRequest, res: Response) => {
     // const {id} = req.params
     const { user } = req;
 
@@ -26,6 +26,7 @@ export const users = (webevApp: WebevApp): Router => {
 
     try {
       console.log('user');
+      console.log(user._id);
       const userPage = await useCase.execute(user._id);
       return res.status(200).json(userPage);
     } catch (err) {
