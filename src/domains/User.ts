23382b0e2af0c552c36eb918f81a1e5f7035365f
itizawa <itizawa@weseek.co.jp>
2021-06-11 22:58:44 +0900

    update domain

--- src/domains/User.ts
+++ src/domains/User.ts
@@ -3,13 +3,15 @@ export class User {
   name: string;
   email: string;
   image: string;
+  admin: boolean;
   createdAt: Date;
   updatedAt: Date;
-  constructor({ _id, name, email, image, createdAt, updatedAt }: User) {
+  constructor({ _id, name, email, image, admin, createdAt, updatedAt }: User) {
     this._id = _id;
     this.name = name;
     this.email = email;
     this.image = image;
+    this.admin = admin;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
   }
