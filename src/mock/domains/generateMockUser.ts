e84b27effda75db83ed5d739210f3cce18230480
itizawa <itizawa@weseek.co.jp>
2021-06-11 23:34:27 +0900

    update generateMockUser

--- src/mock/domains/generateMockUser.ts
+++ src/mock/domains/generateMockUser.ts
@@ -1,12 +1,13 @@
 import { User } from '../../domains/User';
 
 export const generateMockUser = (mock: Partial<User> = {}): User => {
-  const { _id, name, email, image, createdAt, updatedAt } = mock;
+  const { _id, name, email, image, admin, createdAt, updatedAt } = mock;
   return new User({
     _id: _id || 'mockUserId',
     name: name || 'mockUserName',
     email: email || 'mockUserEmail',
     image: image || 'mockUserImage',
+    admin: admin || false,
     createdAt: createdAt || new Date('2020-01-01'),
     updatedAt: updatedAt || new Date('2020-01-01'),
   });
