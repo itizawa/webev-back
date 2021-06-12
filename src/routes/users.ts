a8b7ce9d50c82301b988d41536197c5ebb5083a1
zahmis <meda829ratteru86@gmail.com>
2021-06-12 12:33:08 +0900

    add updagte-info

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,7 @@
 import { Router, Response } from 'express';
 // import { body, param, query } from 'express-validator';
 // import { apiValidatorMiddleware } from '../middlewares/api-validator';
+import { UpdateUserInfoById } from 'src/usecases/user/UpdateUserInfoUseCase';
 import { loginRequired } from '../middlewares/login-required';
 import { accessTokenParser } from '../middlewares/access-token-parser';
 
@@ -21,6 +22,20 @@ export const users = (): Router => {
 
     try {
       const userPage = await useCase.execute(user._id);
+      return res.status(200).json(userPage);
+    } catch (err) {
+      return res.status(500).json({ message: err.message });
+    }
+  });
+
+  router.put('/me/update-info', accessTokenParser, loginRequired, async (req: WebevRequest, res: Response) => {
+    const { user } = req;
+
+    const userRepository = new UserRepository();
+    const useCase = new UpdateUserInfoById(userRepository);
+
+    try {
+      const userPage = await useCase.execute(user._id, user.name);
       return res.status(200).json(userPage);
     } catch (err) {
       return res.status(500).json({ message: err.message });
