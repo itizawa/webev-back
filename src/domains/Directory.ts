494af7d72386eb38fccc6350b1cdae30d3a9315c
kaori <kaori@weseek.co.jp>
2021-06-20 12:56:15 +0900

    FB

--- src/domains/Directory.ts
+++ src/domains/Directory.ts
@@ -8,14 +8,14 @@ export class Directory {
   createdAt: Date;
   updatedAt: Date;
   emojiId: string;
-  constructor({ _id, name, order, createdUser, isRoot, description, createdAt, updatedAt }: Directory) {
+  constructor({ _id, name, order, createdUser, isRoot, description, emojiId, createdAt, updatedAt }: Directory) {
     this._id = _id;
     this.name = name;
     this.order = order;
     this.createdUser = createdUser;
     this.isRoot = isRoot;
     this.description = description;
-    this.emojiId = 'open_file_folder';
+    this.emojiId = emojiId;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
   }
