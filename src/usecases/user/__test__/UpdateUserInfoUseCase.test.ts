d23d9dd7475069265e8c4b3dd605db75790ac6b1
zahmis <meda829ratteru86@gmail.com>
2021-06-20 20:10:53 +0900

    add test

--- src/usecases/user/__test__/UpdateUserInfoUseCase.test.ts
+++ src/usecases/user/__test__/UpdateUserInfoUseCase.test.ts
@@ -16,5 +16,12 @@ describe('UpdateUserInfoById', () => {
 
     expect(spy).toHaveBeenCalled();
     expect(response.name).toBe('hoge');
+    expect(response._id).toBe(mockUser._id);
+    expect(response.email).toBe(mockUser.email);
+    expect(response.image).toBe(mockUser.image);
+    expect(response.createdAt).toBe(mockUser.createdAt);
+    expect(response.admin).toBe(mockUser.admin);
+    expect(response.updatedAt).not.toBe(mockUser.updatedAt);
+    expect(response.isExecutedTutorial).toBe(mockUser.isExecutedTutorial);
   });
 });
