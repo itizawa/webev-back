6d332bf9f907ed0a9a020dd2407fea2b346bc753
zahmis <meda829ratteru86@gmail.com>
2021-06-20 18:47:03 +0900

    create FindUserPageUseCase

---
+++ src/usecases/user/__test__/FindUserPageUseCase.test.ts
@@ -0,0 +1,21 @@
+import { generateMockUser, generateMockPage } from '../../../mock/domains';
+import { UserRepositoryMock } from '../../../mock/repositories';
+import { FindUserPageUseCase } from '../FindUserPageUseCase';
+
+describe('FindUserPageUseCase', () => {
+  const mockUser = generateMockUser();
+  //   const mockPage = generateMockPage();
+  const mock = new UserRepositoryMock();
+
+  mock.findUserById = async (userId) => generateMockUser({ _id: userId });
+
+  const useCase = new FindUserPageUseCase(mockUser._id);
+  const spy = jest.spyOn(mock, 'findUserById');
+
+  test('isFindUserPage', async () => {
+    const response = await useCase.execute(mockUser._id);
+
+    expect(spy).toHaveBeenCalled();
+    // expect(response._id).toBe(mockPage._id);
+  });
+});
