fc09a1352adaf52bc27ac564fac85d1ce3f5de5b
itizawa <itizawa@weseek.co.jp>
2021-07-03 22:32:59 +0900

    implement Article domain

---
+++ src/domains/Article.ts
@@ -0,0 +1,20 @@
+export class Article {
+  _id: string;
+  title: string; // Directory の name に相当
+  body: string; // Directory の description に相当
+  createdUser: string;
+  isPublic: boolean;
+  emojiId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  constructor({ _id, title, createdUser, isPublic, body, emojiId, createdAt, updatedAt }: Article) {
+    this._id = _id;
+    this.title = title;
+    this.createdUser = createdUser;
+    this.isPublic = isPublic;
+    this.body = body;
+    this.emojiId = emojiId;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+}
