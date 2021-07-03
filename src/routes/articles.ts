e8ba28fe71b789b10464d95aef1778ea921e841b
itizawa <itizawa@weseek.co.jp>
2021-07-03 23:59:29 +0900

    implement route

---
+++ src/routes/articles.ts
@@ -0,0 +1,38 @@
+import { Router, Response } from 'express';
+import { adminRequired } from '../middlewares/admin-required';
+import { accessTokenParser } from '../middlewares/access-token-parser';
+
+import { WebevRequest } from '../interfaces/webev-request';
+
+import { CreateArticleUseCase } from '../usecases/article/CreateArticleUseCase';
+
+const router = Router();
+
+import { Article } from '../domains/Article';
+import { factory } from '../repositories/factory';
+const articleRepository = factory.articleRepository();
+
+type PostArticle = {
+  body: {
+    article: Partial<Article>;
+  };
+};
+
+export const articles = (): Router => {
+  router.post('/', accessTokenParser, adminRequired, async (req: WebevRequest & PostArticle, res: Response) => {
+    const { user } = req;
+    const { article } = req.body;
+    article.createdUser = user._id;
+
+    const useCase = new CreateArticleUseCase(articleRepository);
+
+    try {
+      const result = await useCase.execute({ article });
+      return res.status(200).json(result);
+    } catch (err) {
+      return res.status(500).json({ message: err.message });
+    }
+  });
+
+  return router;
+};
