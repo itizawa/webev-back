8272d7ed0385aeffcad71994bdfc85a31426a130
itizawa <itizawa@weseek.co.jp>
2021-07-04 01:03:57 +0900

    update ock

--- src/mock/repositories/ArticleRepositoryMock.ts
+++ src/mock/repositories/ArticleRepositoryMock.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Article } from '../../domains/Article';
+import { Article, UpdatableProperity } from '../../domains/Article';
 import { IArticleRepository } from '../../repositories/IArticleRepository';
 
 export class ArticleRepositoryMock implements IArticleRepository {
   createArticle({ article }: { article: Article }): Promise<Article> {
     throw new Error('Method not implemented.');
   }
-  updateArticle({ article }: { article: Article }): Promise<Article> {
+  updateArticle({ articleId, properity }: { articleId: string; properity: UpdatableProperity }): Promise<Article> {
     throw new Error('Method not implemented.');
   }
 }
