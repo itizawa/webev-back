ccc2264f60f223a90357b601a9bcc540307fe89f
kaori <kaori@weseek.co.jp>
2021-07-17 00:59:42 +0900

    notification repo

---
+++ src/repositories/INotificationRepository.ts
@@ -0,0 +1,8 @@
+import { Notification, NotificationType } from '../domains/Notification';
+
+export interface INotificationRepository {
+  //   createScrap({ scrap }: { scrap: Partial<Scrap> }): Promise<Scrap>;
+  //   deleteScrap({ scrapId, userId }: { scrapId: string; userId: string }): Promise<Scrap>;
+  //   findScrapById({ scrapId }: { scrapId: string }): Promise<Scrap>;
+  //   updateScrap({ scrapId, property, userId }: { scrapId: string; property: UpdatableProperty; userId: string }): Promise<Scrap>;
+}
